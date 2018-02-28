034369f245526a1c8e6defcd47677738417a0031
Gaël Poupard <gael.poupard@orange.com>
2018-02-28 11:09:34 +0100

    feat: add a :focus-within polyfill with feature testing +  CSS progressive enhancement based on clip

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,68 @@
-"use strict";
+(function () {
+  "use strict";
+  
+  // Generic function to test selector support
+  // Modernizr style
+  var supports = function supports() {
+    try {
+      document.querySelector(":focus-within")
+    } catch (error) {
+      return false;
+    }
+    return true;
+  }
+	
+	// :focus-within polyfill
+	// @link https://gist.github.com/aFarkas/a7e0d85450f323d5e164
+	var focusWithin = (function() {
+		var running, last;
+		var action = function() {
+			var element = document.activeElement;
+			running = false;
+			if (last !== element) {
+				last = element;
+				[].slice.call(document.getElementsByClassName('focus-within')).forEach(function(el){
+      		el.classList.remove('focus-within');
+      	});
+				while (element && element.classList) {
+					element.classList.add('focus-within');
+					element = element.parentNode;
+				}
+			}
+		};
+		return function() {
+			if (!running) {
+				requestAnimationFrame(action);
+				running = true;
+			}
+		};
+	})();
+  
+	// Add class on <html> when :focus-within is unsupported
+  var onLoad = function onLoad() {
+    if (supports() !== true) {
+			document.addEventListener('focus', focusWithin, true);
+			document.addEventListener('blur', focusWithin, true);
+			focusWithin();
+    }
+    
+    document.removeEventListener("DOMContentLoaded", onLoad);
+  };
 
-document.addEventListener("DOMContentLoaded", function () {
-  var details = document.querySelectorAll('.vertical details');
+  document.addEventListener("DOMContentLoaded", onLoad);
+  
+  // A single vertical <details> opened at the same time
+  document.addEventListener("DOMContentLoaded", function () {
+    var details = document.querySelectorAll('.vertical details');
 
-  details.forEach(function (target) {
-    target.addEventListener("click", function () {
-      details.forEach(function (detail) {
-        if (detail !== target) {
-          detail.removeAttribute("open");
-        }
+    details.forEach(function (target) {
+      target.addEventListener("click", function () {
+        details.forEach(function (detail) {
+          if (detail !== target) {
+            detail.removeAttribute("open");
+          }
+        });
       });
     });
   });
-});
+})(document);
