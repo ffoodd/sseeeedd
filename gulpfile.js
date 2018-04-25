315876d26d244f5159ecd04bc99450dee01a45f6
Gaël Poupard <gael.poupard@orange.com>
2018-04-25 12:04:56 +0200

    feat: new Travis gulp task to exit on HTML validation error

--- gulpfile.js
+++ gulpfile.js
@@ -341,7 +341,7 @@ gulp.task('louis', function() {
       domContentLoaded: 1500,
       timeToFirstByte: 500,
       DOMelementMaxDepth: 8,
-      requests: 5,
+      requests: 6,
       webfontSize: 300,
       webfontCount: 5,
       notFound: 0,
@@ -380,4 +380,43 @@ gulp.task('zip', ['copy'], function () {
     return gulp.src('./docs/**/*.*')
         .pipe(zip('pkg.zip'))
         .pipe(gulp.dest('./'));
+});
+
+
+/**
+ * @section Test
+ * Travis
+ */
+gulp.task('travis', function() {
+  fs.readFile(test.grps, 'utf8', (error, response) => {
+    if (error) {
+      throw error;
+    }
+
+    html({data: response})
+      .then((data) => {
+        let messages = JSON.parse(data);
+        let errors = [];
+        let results  = messages.messages.reduce((results, value, key) => { 
+          results[key] = value; 
+          return results; 
+        }, {});
+        for (let result in results) {
+          if (results[result].type == "error") {
+            errors.push(results[result]);
+          }
+        }
+        if (errors.length > 0) {
+          errors.forEach(function(error) {
+            console.error(`${error.message} from line ${error.firstLine}, column ${error.firstColumn}; to line ${error.lastLine}, column ${error.lastColumn}`)
+          });
+           process.exit(1);
+        } else {
+          console.log('The HTML document validates according to the W3C')
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    })
 });
