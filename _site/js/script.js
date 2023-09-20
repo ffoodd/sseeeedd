b6bfa287040354a3ee56bb9c7ef875ea6db140ad
Gaël Poupard <gael+github@ffoodd.fr>
2023-09-20 12:12:44 +0200

    fix(high contrast): remove theme switcher when high contrast

--- _site/js/script.js
+++ _site/js/script.js
@@ -66,35 +66,36 @@
 	});
 
 	// Dark mode
-	// @todo Disable all the things if (prefers-contrast: more) ?
-	const switcher = document.getElementById('theme');
-	const options = switcher.querySelectorAll('button');
-	let button = document.querySelector('[data-scheme="none"]');
-	//// Then check for localStorage
-	const currentTheme = localStorage.getItem('theme');
-	if (currentTheme) {
-		document.documentElement.dataset.theme = currentTheme;
-		if (currentTheme === 'dark') {
-			button = document.querySelector('[data-scheme="dark"]');
-		} else if (currentTheme === 'light') {
-			button = document.querySelector('[data-scheme="light"]');
-		}
-	}
-	button.setAttribute('aria-pressed', 'true');
-
-	//// Finally handle overriding through buttons
-	options.forEach(option => {
-		option.addEventListener('click', () => {
-			switcher.querySelector('[aria-pressed="true"]').setAttribute('aria-pressed', 'false');
-			option.setAttribute('aria-pressed', 'true');
-			if (option.dataset.scheme === 'none') {
-				delete document.documentElement.dataset.theme;
-				localStorage.removeItem('theme');
-			} else {
-				document.documentElement.dataset.theme = option.dataset.scheme;
-				localStorage.setItem('theme', option.dataset.scheme);
+	if (! window.matchMedia('(prefers-contrast: more)').matches) {
+		const switcher = document.getElementById('theme');
+		const options = switcher.querySelectorAll('button');
+		let button = document.querySelector('[data-scheme="none"]');
+		//// Then check for localStorage
+		const currentTheme = localStorage.getItem('theme');
+		if (currentTheme) {
+			document.documentElement.dataset.theme = currentTheme;
+			if (currentTheme === 'dark') {
+				button = document.querySelector('[data-scheme="dark"]');
+			} else if (currentTheme === 'light') {
+				button = document.querySelector('[data-scheme="light"]');
 			}
+		}
+		button.setAttribute('aria-pressed', 'true');
+
+		//// Finally handle overriding through buttons
+		options.forEach(option => {
+			option.addEventListener('click', () => {
+				switcher.querySelector('[aria-pressed="true"]').setAttribute('aria-pressed', 'false');
+				option.setAttribute('aria-pressed', 'true');
+				if (option.dataset.scheme === 'none') {
+					delete document.documentElement.dataset.theme;
+					localStorage.removeItem('theme');
+				} else {
+					document.documentElement.dataset.theme = option.dataset.scheme;
+					localStorage.setItem('theme', option.dataset.scheme);
+				}
+			});
 		});
-	});
+	}
 
 })(document);
